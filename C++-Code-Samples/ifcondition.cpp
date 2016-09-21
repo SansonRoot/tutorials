d2057bf6cd23fb7508797626edab2faa68a7b9dd
sansonroot <asanyuo123@gmail.com>
2016-09-21 18:50:04 +0000

    added if condition

---
+++ C++-Code-Samples/ifcondition.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int age;
+	string name;
+	
+	cout<<"Please enter your name :";
+	cin>>name;
+	
+	cout<<"Please enter your right age : ";
+	cin>>age;
+	
+	if(age <= 0){
+		cout<<"\nPlease enter a valid age !"<<endl;
+	}
+	
+	if( age < 18){
+		cout<<"\nPlease Mr. "<<name<<" , you arent of age "<<endl;
+	}
+	
+	if( age >= 18){
+		cout<<"\nWelcome Mr. "<<name<<" , what may we serve you...?"<<endl;
+	}
+	
+	return 0;
+}
