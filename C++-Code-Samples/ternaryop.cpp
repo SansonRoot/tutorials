7308ab250d0d57fee47d1ad1f6981bf07f52b829
sansonroot <asanyuo123@gmail.com>
2016-09-24 12:20:07 +0000

    ternary and exercise 1

---
+++ C++-Code-Samples/ternaryop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a = 8;
+	int b = 2;
+	
+	((a/4) == b) ? cout<<"They are equal " : cout<<"They arent equal";
+	
+	
+	
+	return 0;
+}
