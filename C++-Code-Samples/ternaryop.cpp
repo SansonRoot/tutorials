8dadf2e2134e0314e7ca3df03305f8b71eea624b
sansonroot <asanyuo123@gmail.com>
2016-09-24 12:36:37 +0000

    updated ternary

--- C++-Code-Samples/ternaryop.cpp
+++ C++-Code-Samples/ternaryop.cpp
@@ -7,9 +7,8 @@ int main(){
 	int a = 8;
 	int b = 2;
 	
-	((a/4) == b) ? cout<<"They are equal " : cout<<"They arent equal";
-	
-	
+	( (a/4) == b ) ? cout<<"They are equal " : cout<<"They arent equal";
+
 	
 	return 0;
 }
