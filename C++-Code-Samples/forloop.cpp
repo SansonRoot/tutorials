1b906d18d8d942ae2629f3ef8afd6ae3be520bbd
sansonroot <asanyuo123@gmail.com>
2016-10-01 19:15:48 +0000

    added forloop

---
+++ C++-Code-Samples/forloop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a;
+	
+	for(a = 0; a <= 100; a += 2){
+		cout<<a<<"\n";
+		//a++;	
+	}
+	
+	cout<<"\na = "<<a<<"\n";
+			
+	return 0;
+}
